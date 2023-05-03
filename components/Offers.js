dc07b0277697fa1ab692947507ef6f1c3a45069f
natasha-pereira <nat.pereira167@gmail.com>
2023-05-03 13:50:08 +0530

    Added new component

---
+++ components/Offers.js
@@ -0,0 +1,18 @@
+import "./comp.css";
+
+const Offers = () => {
+	return (
+		<div className="offer-banner">
+			<div className="offer-banner-content">
+				<h1>Offers for you!</h1>
+				<p>Explore top deals and offers exclusively for you! 🥂</p>
+			</div>
+			<img
+				alt="offer logo"
+				src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/KHu24Gqw_md3ham"
+			/>
+		</div>
+	);
+};
+
+export default Offers;
